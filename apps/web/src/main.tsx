8c7986e4b271a9e22e2e0ff60a9524f79ebc484d
shakethat01 <steam6242@gmail.com>
2026-09-21 21:52:21 +0300

    Mount global user context menu

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import { GlobalUserContext } from './GlobalUserContext';
 import './styles.css';
 import './shakechatbot-theme.css';
 import './voice-theater.css';
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>);
+import './user-context.css';
+ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /><GlobalUserContext /></React.StrictMode>);
