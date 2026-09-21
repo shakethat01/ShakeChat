2de1e4f3cf56f024d103265d05b59b5de966aa69
shakethat01 <steam6242@gmail.com>
2026-09-21 22:47:30 +0300

    Register shared-server DM access service

--- apps/api/src/direct-messages/direct-messages.module.ts
+++ apps/api/src/direct-messages/direct-messages.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AuthGuard } from '../auth/auth.guard';
 import { MessagesModule } from '../messages/messages.module';
+import { DirectMessageAccessService } from './direct-message-access.service';
 import { DirectMessagesController } from './direct-messages.controller';
 import { DirectMessagesService } from './direct-messages.service';
 
 @Module({
   imports: [MessagesModule],
   controllers: [DirectMessagesController],
-  providers: [DirectMessagesService, AuthGuard],
+  providers: [DirectMessagesService, DirectMessageAccessService, AuthGuard],
   exports: [DirectMessagesService],
 })
 export class DirectMessagesModule {}
