3de1dd356cc1355ab467283f42773de89be53d45
shakethat01 <steam6242@gmail.com>
2026-09-24 11:26:59 +0300

    install ShakeChat microphone gain policy

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './microphoneCapturePolicy';
 import './screenAudioCapture';
 import { App } from './App';
 import { GlobalUserContext } from './GlobalUserContext';
